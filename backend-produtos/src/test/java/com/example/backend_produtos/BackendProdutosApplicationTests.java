53c6f59f7f1053f325ac3cdf8565dc7734ac35b8
JoseVinicius05 <joseviniciusgrecco05@gmail.COM>
2025-01-03 22:22:41 -0300

    projeto back e front

---
+++ backend-produtos/src/test/java/com/example/backend_produtos/BackendProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
